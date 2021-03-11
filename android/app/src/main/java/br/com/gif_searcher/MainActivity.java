39306399ead86263d3b33ae8cd553bda15167519
VitorRomanelli <vitorvromanelli@gmail.com>
2021-03-11 17:32:14 -0300

    App Finished

---
+++ android/app/src/main/java/br/com/gif_searcher/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.gif_searcher;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:gif_searcher/Views/gif_page.dart';
+import 'Views/home.dart';
+import 'Views/gif_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
